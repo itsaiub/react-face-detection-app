5ac84305225ec8692319307bab0e7fbb6bfc8cb5
itsaiub <itsaiub@gmail.com>
2019-05-09 00:15:45 +0600

    Nav, Logo, Rank and Navigation created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header" />
-    </div>
-  );
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import Rank from "./components/Rank/Rank";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*  <FaceReconition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
