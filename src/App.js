f96ac059cb311fbdf3de08bd063c38cdaa4250ff
itsaiub <itsaiub@gmail.com>
2019-05-09 23:35:38 +0600

    sign in and routing implement

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import SignIn from "./components/SignIn/SignIn";
+import Register from "./components/Register/Register";
 
 const app = new Clarifai.App({
   apiKey: "8a01c5a0366e47429a9fc3f06d02f60f"
@@ -18,7 +20,9 @@ class App extends React.Component {
   state = {
     input: "",
     imgUrl: "",
-    box: {}
+    box: {},
+    route: "signin",
+    isSingedIn: false
   };
 
   calculateFaceLocation = data => {
@@ -53,19 +57,39 @@ class App extends React.Component {
       .catch(err => console.log(err));
   };
 
+  handleRouteChange = route => {
+    if (route === "signout") {
+      this.setState({ isSingedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSingedIn: true });
+    }
+    this.setState({ route: route });
+  };
+
   render() {
-    const { box, imgUrl } = this.state;
+    const { box, imgUrl, route, isSingedIn } = this.state;
     return (
       <div className="App">
         <Particles className="particles" params={particleOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.handleInputChange}
-          onSubmit={this.handleSubmit}
+        <Navigation
+          onRouteChange={this.handleRouteChange}
+          isSingedIn={isSingedIn}
         />
-        <FaceRecognition box={box} imageUrl={imgUrl} />
+        {route === "home" ? (
+          <React.Fragment>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onSubmit}
+            />
+            <FaceRecognition box={box} imageUrl={imgUrl} />
+          </React.Fragment>
+        ) : route === "signin" ? (
+          <SignIn onRouteChange={this.handleRouteChange} />
+        ) : (
+          <Register onRouteChange={this.handleRouteChange} />
+        )}
       </div>
     );
   }
@@ -74,7 +98,7 @@ class App extends React.Component {
 const particleOptions = {
   particles: {
     number: {
-      value: 50,
+      value: 70,
       density: {
         enable: true,
         value_area: 800
